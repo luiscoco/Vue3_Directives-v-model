89afad265a1e51e37f8fa7a09e12ebedc0370727
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-06-12 18:21:01 +0200

    third-commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <v_model msg="Welcome to Your Vue.js + TypeScript App" />
+  <v_directives msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import v_model from "./components/v_model.vue";
+import v_directives from "./components/v_directives.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    v_model,
+    v_directives,
   },
 });
 </script>

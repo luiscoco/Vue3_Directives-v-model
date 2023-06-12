a3b833c19693b3ec2aa71a3bffa5ea52af02d6b8
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-06-12 16:39:32 +0200

    first-commit

---
+++ src/components/v_model.vue
@@ -0,0 +1,9 @@
+<template>
+  <input v-model="message" type="text" />
+  <p>{{ message }}</p>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+const message = ref("");
+</script>
